39d8f37498b94e806d6a7cc3c3ddd0deeb9e5ff2
Duc Manh <ducmanhtb2802@gmail.com>
2021-06-22 13:31:35 +0700

    update

--- films/src/main/java/com/manhdd/film/controller/FilmController.java
+++ films/src/main/java/com/manhdd/film/controller/FilmController.java
@@ -16,6 +16,8 @@ public class FilmController {
     public String listBooks(Model model) {
         List<Film> films = List.of(new Film("Titanic", "Lang man", "James Cameron", 1992),
                 new Film("Game of Thrones", "hanh dong", " David Benioff ", 2021),
+                new Film("Toys", "hoat hinh", " David Osspina ", 2021),
+                new Film("Tom and jerry", "cartoon", " David Beckham ", 2021),
                 new Film("Fast and Fourious", "hanh dong", "Justin Lin", 2020));
         model.addAttribute("films", films);
         return "film";
